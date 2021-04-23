c2ef6e2130ad3fe57162f014dd761d6718fd8e16
mf <mikailfidan38@gmail.com>
2021-04-23 03:37:59 +0300

    add controller

---
+++ app/Http/Controllers/Trade.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Trade extends Controller
+{
+    public function index()
+    {
+
+    }
+}
---
+++ app/Trader/Trader.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Trader;
+
+
+class Trader
+{
+
+}
