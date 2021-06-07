c157a598e89c38c836403a273a00eed41bbf8447
milkymeda <mikailfidan38@gmail.com>
2021-06-07 16:43:52 +0300

    add map function, refactor

--- app/Models/Candles.php
+++ app/Models/Candles.php
@@ -11,6 +11,9 @@ use Illuminate\Database\Eloquent\Model;
  * @property string exchange
  * @property array  data
  * @property array  map
+ * @property int    length
+ * @property int    start_date
+ * @property int    end_date
  * @property mixed  created_at
  * @property mixed  updated_at
  */
@@ -18,19 +21,65 @@ class Candles extends Model
 {
     use HasFactory;
 
-    const MAX_DATA_LENGTH = 1000;
-
     protected $table = 'candles';
 
-    public function __get($key)
+    const MAX_CANDLE_LENGTH = 1000;
+
+    protected $casts = [
+        'data' => 'array',
+        'map' => 'array'
+    ];
+
+    protected $firstKey;
+    protected $lastKey;
+
+    protected static function booted()
     {
-        $column = $this->map[$key] ?? null;
+        static::saving(function (self $candles) {
+            if ($candles->data)
+            {
+                $candles->start_date = $candles->first()[$candles->map('open')];
+                $candles->end_date = $candles->last()[$candles->map('close')];
+            }
 
-        if($column !== null)
-        {
-            return array_column($this->data, $column);
-        }
+            $candles->length = $candles->length();
+        });
+    }
 
-        return parent::__get($key);
+    public function getLengthAttribute(): int
+    {
+        return $this->length();
+    }
+
+    public function setDataAttribute(array $value)
+    {
+        $this->attributes['data'] = $value;
+        $this->updateKeys();
+    }
+
+    public function last(): array
+    {
+        return $this->attributes['data'][$this->lastKey];
+    }
+
+    public function first(): array
+    {
+        return $this->attributes['data'][$this->firstKey];
+    }
+
+    public function length(): int
+    {
+        return count($this->attributes['data']);
+    }
+
+    public function map($key)
+    {
+        return $this->attributes['map'][$key];
+    }
+
+    protected function updateKeys()
+    {
+        $this->firstKey = array_key_first($this->attributes['data']);
+        $this->lastKey = array_key_last($this->attributes['data']);
     }
 }
