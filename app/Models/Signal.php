50af2794d1180e97222b903255d1c1a052b4f0a1
milkymeda <mikailfidan38@gmail.com>
2021-06-13 04:37:38 +0300

    Create Signal model

--- app/Models/Signal.php
+++ app/Models/Signal.php
@@ -8,9 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 /** COLUMNS
  *
  * @property int    id
- * @property string name
- * @property string desc
- * @property array  config
+ * @property int    trade_setup_id
+ * @property string type
+ * @property string indicator
+ * @property int    indicator_version
+ * @property string side
+ * @property string symbol
+ * @property string interval
+ * @property float  price
  * @property mixed  created_at
  * @property mixed  updated_at
  */
