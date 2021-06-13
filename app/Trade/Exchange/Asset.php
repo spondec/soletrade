59401171865dfb8b4b8cdbf1128db5f5c2b71002
milkymeda <mikailfidan38@gmail.com>
2021-06-13 04:39:29 +0300

    Add getters

--- app/Trade/Exchange/Asset.php
+++ app/Trade/Exchange/Asset.php
@@ -5,8 +5,28 @@ namespace App\Trade\Exchange;
 class Asset
 {
     public function __construct(protected string $symbol,
-                                protected float $amount,
-                                protected float $available)
+                                protected float  $amount,
+                                protected float  $available)
+    {
+
+    }
+
+    public function symbol(): string
+    {
+        return $this->symbol;
+    }
+
+    public function total(): float
+    {
+        return $this->amount;
+    }
+
+    public function available(): float
+    {
+        return $this->available;
+    }
+
+    public function valueAsCurrency(string $currency): float
     {
 
     }
