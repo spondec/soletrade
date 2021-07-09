9a358af3d575dd06d3bd70dcfc8839fc833157a6
milkymeda <mikailfidan38@gmail.com>
2021-07-09 08:29:24 +0300

    Remove http request methods, request url constants, add symbol arg to openOrders()

--- app/Trade/Exchange/AbstractExchange.php
+++ app/Trade/Exchange/AbstractExchange.php
@@ -9,10 +9,6 @@ use Illuminate\Support\Facades\Http;
 
 abstract class AbstractExchange
 {
-    const ROOT_URI = null;
-    const NEW_ORDER_URL = null;
-    const ORDER_UPDATE_URL = null;
-
     protected string $apiKey;
     protected string $secretKey;
 
@@ -30,14 +26,9 @@ abstract class AbstractExchange
 
     private function __construct()
     {
-//        if (empty(static::ROOT_URI) || empty(static::NEW_ORDER_URL))
-//        {
-//            throw new \InvalidArgumentException('Exchange constants are not properly defined.');
-//        }
+        $this->name = mb_strtoupper(class_basename(static::class));
 
-        $this->name = class_basename(static::class);
-
-        $config = Config::get('exchange.' . mb_strtoupper($this->name));
+        $config = Config::get('exchange.' . $this->name);
 
         if (empty($config['apiKey']) || empty($config['secretKey']))
         {
@@ -49,6 +40,13 @@ abstract class AbstractExchange
 
         $this->actions = $this->availableOrderActions();
         $this->account = $this->accountType();
+
+        $this->setup();
+    }
+
+    protected function setup(): void
+    {
+
     }
 
     public function price(string $symbol): float
@@ -85,13 +83,18 @@ abstract class AbstractExchange
         $order->exchange = $this->name;
         $order->account = $this->account;
 
-        if (!in_array($order->side, $this->actions))
-        {
-            throw new \UnexpectedValueException("$this->name exchange doesn't allow to $order->side.\n
-            Available actions: " . implode(', ', $this->actions));
-        }
+        $this->assertAction($order);
 
         return $order;
+    }
+
+    protected final function assertAction(Order $order): void
+    {
+        if (!in_array($order->side, $this->actions))
+        {
+            throw new \UnexpectedValueException("$this->name doesn't allow to $order->side.\n
+            Available actions: " . implode(', ', $this->actions));
+        }
     }
 
     public function market(string $side, string $symbol, float $quantity): Order
@@ -131,7 +134,7 @@ abstract class AbstractExchange
     /**
      * @return Order[]
      */
-    abstract public function openOrders(): array;
+    abstract public function openOrders(string $symbol): \Illuminate\Database\Eloquent\Collection;
 
     abstract public function accountBalance(): AccountBalance;
 
@@ -146,48 +149,32 @@ abstract class AbstractExchange
 
     abstract public function candles(string $symbol, string $interval, float $start, float $end): array;
 
-    protected function httpRequest(string $method, string $url, array $data): array
-    {
-        /** @var Response $response */
-        $response = call_user_func([Http::class, $method], $url,
-            array_merge($data, $this->getDefaultRequestData()));
-
-        return $response->json() ?? throw new \HttpResponseException(
-                "Empty result received from request $method:$url.");
-    }
-
-    protected function hmacSha256(string $key, array $data): string
-    {
-        return hash_hmac('sha256', http_build_query($data), $key);
-    }
-
     protected function newOrder(Order $order): Order
     {
-        $request = $this->prepareNewOrderRequest($order);
-        $order->logRequest('newOrder', $request);
-
-        $response = $this->httpRequest('post', self::NEW_ORDER_URL, $request);
+        $response = $this->executeNewOrder($order);
         $order->logResponse('newOrder', $response);
 
         $this->handleNewOrderResponse($order, $response);
 
+        $order->save();
+
         return $order;
     }
+
+    abstract protected function updateOrderDetails(Order $order, array $response): void;
 
     abstract protected function prepareCancelOrderRequest(Order $order): array;
 
     abstract protected function handleCancelOrderResponse(Order $order, array $response): void;
 
-    abstract protected function prepareNewOrderRequest(Order $order): array;
+    abstract protected function executeNewOrder(Order $order): array;
 
     abstract protected function handleNewOrderResponse(Order $order, array $response): void;
 
     abstract protected function prepareOrderUpdateRequest(Order $order): array;
 
     abstract protected function handleOrderUpdateResponse(Order $order, array $response): void;
-
-    abstract protected function getDefaultRequestData(): array;
-
+    
     abstract protected function accountType(): string;
 
     public static function instance(): static
