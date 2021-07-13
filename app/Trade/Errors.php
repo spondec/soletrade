531c3b5cc6855a47e2f17c3f51d7c6f31e82312e
milkymeda <mikailfidan38@gmail.com>
2021-07-13 11:46:29 +0300

    Create error log class

---
+++ app/Trade/Errors.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Trade;
+
+final class Errors
+{
+    protected static array $log;
+
+    public static function log(string|\Exception $message): void
+    {
+        if ($message instanceof \Exception)
+        {
+            $exception = get_class($message);
+            $message = $message->getMessage();
+        }
+
+        static::$log[] = [
+            'time' => microtime(true),
+            'exception' => $exception ?? null,
+            'message' => $message ?? 'Empty message received.'
+        ];
+    }
+}
