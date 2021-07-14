15f8e34461d31d005a5d8d942b533e9ad4876035
milkymeda <mikailfidan38@gmail.com>
2021-07-15 02:51:32 +0300

    Implemented: calculate relative asset worth

--- app/Trade/Exchange/AccountBalance.php
+++ app/Trade/Exchange/AccountBalance.php
@@ -2,21 +2,65 @@
 
 namespace App\Trade\Exchange;
 
+use App\Trade\Errors;
+
 class AccountBalance
 {
     /** @var Asset[] */
     protected array $assets;
 
-    public function __construct(array $assets)
+    public function __construct(protected AbstractExchange $exchange,
+                                array                      $assets,
+                                protected string           $relativeAsset = 'USDT')
     {
         foreach ($assets as $asset)
         {
-            if(!$asset instanceof Asset)
+            if (!$asset instanceof Asset)
             {
                 throw new \InvalidArgumentException('The argument must be a instance of Asset class.');
             }
+
+            $this->assets[$asset->name()] = $asset;
+        }
+    }
+
+    protected function calculateRelativeWorth()
+    {
+        $list = $this->exchange->symbolList($this->relativeAsset);
+        $relativeWorth = [];
+
+        foreach ($this->assets as $asset)
+        {
+            if (($baseAsset = $asset->name()) != $this->relativeAsset)
+            {
+                $symbol = $this->exchange->buildSymbol($baseAsset, $this->relativeAsset);
+
+                if (!in_array($symbol, $list))
+                {
+                    continue;
+                }
+
+                try
+                {
+                    $price = $this->exchange->orderBook($symbol)->bestAsk();
+
+                } catch (\UnexpectedValueException $e)
+                {
+                    Errors::log($e);
+                    continue;
+                }
+            }
+
+            $relativeWorth[$baseAsset] = ($price ?? 1) * $asset->available();
         }
 
-        $this->assets = $assets;
+        arsort($relativeWorth);
+
+        return $relativeWorth;
+    }
+
+    public function primaryAsset(): Asset
+    {
+        return $this->assets[array_key_first($this->calculateRelativeWorth())];
     }
 }
