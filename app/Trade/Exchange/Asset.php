f1b14e5fb859f40deaccc46af30c8e1df7113b05
milkymeda <mikailfidan38@gmail.com>
2021-07-15 02:50:37 +0300

    Rename symbol to asset

--- app/Trade/Exchange/Asset.php
+++ app/Trade/Exchange/Asset.php
@@ -4,16 +4,16 @@ namespace App\Trade\Exchange;
 
 class Asset
 {
-    public function __construct(protected string $symbol,
+    public function __construct(protected string $asset,
                                 protected float  $amount,
                                 protected float  $available)
     {
 
     }
 
-    public function symbol(): string
+    public function name(): string
     {
-        return $this->symbol;
+        return $this->asset;
     }
 
     public function total(): float
@@ -24,10 +24,5 @@ class Asset
     public function available(): float
     {
         return $this->available;
-    }
-
-    public function currencyValue(string $currency): float
-    {
-
     }
 }
