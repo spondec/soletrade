4741c3b2327c70e5f0d830cc9a049b7ebec8a7d4
milkymeda <mikailfidan38@gmail.com>
2021-07-22 13:39:35 +0300

    Views

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+{{--    <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.js"></script>--}}
+
+<!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <script>
+        (function ()
+        {
+            window.__MF = {};
+            window.__MF.appName = "{{ config('app.name', 'Laravel') }}";
+        })();
+    </script>
+</head>
+<body class="bg-dark">
+<div id="app">
+    <main-layout></main-layout>
+</div>
+</body>
+</html>
