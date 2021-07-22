bd091ec26ad3d6ef9d4a3931a54b12034f2d452c
milkymeda <mikailfidan38@gmail.com>
2021-07-22 13:37:50 +0300

    Config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
