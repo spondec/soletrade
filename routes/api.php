4697312e18b745f0578f95be1b4ab06dadd237fe
milkymeda <mikailfidan38@gmail.com>
2021-07-24 08:52:58 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('orders', [\App\Http\Controllers\Api\OrderController::class, 'index']);
+Route::get('exchanges', [\App\Http\Controllers\Api\ExchangeController::class, 'index']);
+Route::get('exchanges/{exchange}/symbols', [\App\Http\Controllers\Api\ExchangeController::class, 'symbols']);
