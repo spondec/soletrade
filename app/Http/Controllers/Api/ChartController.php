b5ace7f22d6e5b57b840a7a89c96171582951184
milkymeda <mikailfidan38@gmail.com>
2021-07-28 23:06:32 +0300

    Update ChartController.php

--- app/Http/Controllers/Api/ChartController.php
+++ app/Http/Controllers/Api/ChartController.php
@@ -3,15 +3,52 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Model;
+use App\Models\Symbol;
+use App\Trade\Config;
+use App\Trade\Exchange\AbstractExchange;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ChartController extends Controller
 {
-    public function index()
+    public function index(Request $request): array
     {
+        $symbol = $request->get('symbol');
+        $exchange = $request->get('exchange');
+        $interval = $request->get('interval');
+
+        if ($exchange && $symbol && $interval)
+        {
+            return $this->candles($exchange, $symbol, $interval);
+        }
+
+        return [
+            'exchanges'  => Config::exchanges(),
+            'symbols'    => Config::symbols(),
+            'indicators' => Config::indicators(),
+            'intervals'  => DB::table('symbols')->distinct()->get('interval')->pluck('interval')
+        ];
     }
 
-    public function show($id)
+    /**
+     * @param AbstractExchange $exchange
+     */
+    public function candles(string $exchange, string $symbol, string $interval, ?int $before = null): array
     {
+        /** @var Symbol $symbol */
+        /** @noinspection CallableParameterUseCaseInTypeContextInspection */
+        $symbol = Symbol::query()
+            ->where('exchange_id', $exchange::instance()->id())
+            ->where('symbol', $symbol)
+            ->where('interval', $interval)
+            ->first();
+
+        if ($before)
+        {
+            $symbol->candles($before);
+        }
+
+        return $symbol?->toArray() ?? [];
     }
 }
