41ee7e0aa9b3f126dfbc103cd4279a4f666def0c
milkymeda <mikailfidan38@gmail.com>
2021-07-28 23:08:08 +0300

    Update Config.php

--- app/Trade/Config.php
+++ app/Trade/Config.php
@@ -4,14 +4,46 @@ namespace App\Trade;
 
 use App\Trade\Exchange\AbstractExchange;
 use App\Trade\Exchange\Spot\Binance;
+use App\Trade\Indicator\AbstractIndicator;
+use Illuminate\Support\Facades\DB;
 
 final class Config
 {
     /**
-     * @return AbstractExchange[]
+     * @return AbstractExchange[]|string[]
      */
     public static function exchanges(): array
     {
         return [Binance::class];
+    }
+
+    /**
+     * @return AbstractIndicator[]|string[]
+     */
+    public static function indicators(): array
+    {
+        return ['rsi', 'macd', 'fib'];
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function symbols(): array
+    {
+        $symbols = [];
+
+        foreach (static::exchanges() as $exchange)
+        {
+            $exchange = $exchange::instance();
+//            $symbols[$exchange::class] = $exchange->symbols();
+            $symbols[$exchange::class] = DB::table('symbols')
+                ->distinct()
+                ->where('exchange_id', $exchange->id())
+                ->get('symbol')
+                ->pluck('symbol')
+                ->toArray();
+        }
+
+        return $symbols;
     }
 }
