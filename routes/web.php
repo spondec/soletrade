45ca30a64ebd4233eb76c2a40833a023fccbf59e
milkymeda <mikailfidan38@gmail.com>
2021-07-28 23:05:33 +0300

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/{any}', 'home')->where('any', '.*');;
+Route::view('/{any}', 'home')->where('any', '.*');
 
 //Route::get('/artisan', function ()
 //{
