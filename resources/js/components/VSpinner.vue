d5be98e8705eaa8384701245b383d7ba4dbbb952
milkymeda <mikailfidan38@gmail.com>
2021-07-28 23:32:01 +0300

    Create VSpinner.vue

---
+++ resources/js/components/VSpinner.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="d-flex justify-content-center">
+    <div class="spinner-border" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VSpinner"
+}
+</script>
+
+<style scoped>
+
+</style>
