eeb26f3a761c85d55a147934bc4b47d6a24fc27a
milkymeda <mikailfidan38@gmail.com>
2021-08-04 19:25:57 +0300

    Update AbstractStrategy.php

--- app/Trade/Strategy/AbstractStrategy.php
+++ app/Trade/Strategy/AbstractStrategy.php
@@ -4,35 +4,42 @@ namespace App\Trade\Strategy;
 
 use App\Models\Symbol;
 use App\Models\TradeSetup;
+use App\Repositories\SymbolRepository;
 use App\Trade\Indicator\FibonacciRetracement;
 use App\Trade\Indicator\MACD;
 use App\Trade\Indicator\RSI;
-use App\Trade\VersionableInterface;
+use App\Trade\Scanner;
+use App\Trade\Name;
 
-abstract class AbstractStrategy implements VersionableInterface
+abstract class AbstractStrategy implements Name
 {
     const ALLOWED_INTERVALS = [];
 
     /** @var int - seconds */
     const SCAN_INTERVAL = 300;
+    const INTERVALS = [];
+    const CANDLE_LIMIT = 1000;
 
     const INDICATORS = [
-        MACD::class => [],
-        RSI::class => [],
+        MACD::class                 => [],
+        RSI::class                  => [],
         FibonacciRetracement::class => []
     ];
 
-    protected function initIndicators(Symbol $candles): void
+    public function __construct(protected Scanner $scanner, protected SymbolRepository $symbolRepo)
     {
-        foreach (self::INDICATORS as $class => $config)
-        {
-            $candles->addIndicator(new $class($candles, $config));
-        }
     }
 
-    public function check(Symbol $candles): ?TradeSetup
+    public function check(): ?TradeSetup
     {
-        $this->initIndicators($candles);
-
+        foreach (self::INTERVALS as $interval)
+        {
+            foreach ($symbols[$interval] = $this->scanner->scan($interval) as $symbol)
+            {
+                $this->symbolRepo->initIndicators($symbol,
+                    $symbol->candles(limit: static::CANDLE_LIMIT),
+                    static::INDICATORS);
+            }
+        }
     }
 }
