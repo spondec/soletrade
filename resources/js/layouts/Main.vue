7c3f968b376e34c343d2172c4ddb8d8706bd123c
milkymeda <mikailfidan38@gmail.com>
2021-08-04 19:24:54 +0300

    Remove collapse nav button

--- resources/js/layouts/Main.vue
+++ resources/js/layouts/Main.vue
@@ -2,13 +2,8 @@
   <nav class="navbar navbar-expand-md navbar-dark shadow-sm">
     <div class="container">
       <v-link class="navbar-brand" href="/">{{ appName }}</v-link>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent" aria-expanded="false"
-              aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="navbar-collapse">
         <!-- Left Side Of Navbar -->
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
