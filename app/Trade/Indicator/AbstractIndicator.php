f45e0c82ba25b29aa3c81b2b47c5be476a8d57d6
milkymeda <mikailfidan38@gmail.com>
2021-08-25 01:55:49 +0300

    Rearranged

--- app/Trade/Indicator/AbstractIndicator.php
+++ app/Trade/Indicator/AbstractIndicator.php
@@ -21,6 +21,117 @@ abstract class AbstractIndicator
     protected ?int $prev = null;
     protected ?int $current = null;
 
+    /** @var Signature */
+    protected Model $signature;
+
+    protected array $config = [];
+
+    protected array $data = [];
+    /**
+     * @var Signal[]
+     */
+    private Collection $signals;
+    private Signature $signalSignature;
+
+    public function __construct(protected Symbol     $symbol,
+                                protected Collection $candles,
+                                array                $config = [],
+                                protected ?\Closure  $signalCallback = null)
+    {
+        $this->mergeConfig($config);
+
+        $this->signature = $this->register([
+            'contents' => $this->contents()
+        ]);
+
+        $this->signals = new Collection([]);
+
+        if ($candles->count())
+        {
+            $this->data = $this->combineTimestamps($this->run());
+
+            if ($signalCallback)
+            {
+                $this->signalSignature = $this->register([
+                    'config'             => $this->config,
+                    'signalCallbackHash' => ClosureHash::from($this->signalCallback)
+                ]);
+                $this->scan();
+            }
+        }
+    }
+
+    protected function combineTimestamps(?array $data): array
+    {
+        if (!$data)
+        {
+            return [];
+        }
+
+        $timestamps = array_slice($this->timestamps(), ($length = count($data)) * -1, $length);
+
+        return array_combine($timestamps, $data);
+    }
+
+    protected function timestamps(): array
+    {
+        return array_column($this->candles->all(), 't');
+    }
+
+    abstract protected function run(): array;
+
+    protected function scan(): void
+    {
+        foreach ($this->data as $timestamp => $value)
+        {
+            $this->current = $timestamp;
+            if ($signal = $this->checkSignal($timestamp, $value))
+            {
+                $signal->timestamp = $timestamp;
+                $signal->hash = $this->hash(json_encode($signal->attributesToArray()));
+                $existing = Signal::query()->where('hash', $signal->hash)->first();
+
+                if ($existing)
+                {
+                    $signal = $existing;
+                }
+                else
+                {
+                    $signal->save();
+                }
+
+                $this->signals[] = $signal;
+            }
+
+            $this->prev = $timestamp;
+        }
+    }
+
+    protected function checkSignal(int $timestamp, mixed $value): ?Signal
+    {
+        $callback = $this->signalCallback;
+        return $callback(signal: $this->setupSignal(),
+            indicator: $this,
+            timestamp: $timestamp,
+            value: $value);
+    }
+
+    public function setupSignal(): Signal
+    {
+        $signal = new Signal();
+
+        $signal->symbol_id = $this->symbol->id;
+        $signal->indicator_id = $this->id();
+        $signal->signature_id = $this->signalSignature->id;
+
+        return $signal;
+    }
+
+    public function id(): int
+    {
+        return $this->signature->id;
+    }
+
     public function crossOver(\Closure $x, \Closure $y): bool
     {
         if ($this->prev === null || $this->current === null)
@@ -53,96 +164,9 @@ abstract class AbstractIndicator
         return $prevX > $prevY && $currentX < $currentY;
     }
 
-    /** @var Signature */
-    protected Model $signature;
-
-    protected array $config = [];
-    protected array $data = [];
-    /**
-     * @var Signal[]
-     */
-    private Collection $signals;
-    private Signature $signalSignature;
-
-    abstract protected function run(): array;
-
-    public function __construct(protected Symbol     $symbol,
-                                protected Collection $candles,
-                                array                $config = [],
-                                protected ?\Closure  $signalCallback = null)
-    {
-        $this->mergeConfig($config);
-
-        $this->signature = $this->register([
-            'contents' => $this->contents()
-        ]);
-
-        if ($candles->count())
-        {
-            $this->data = $this->combineTimestamps($this->run());
-            $this->signals = new Collection([]);
-
-            if ($signalCallback)
-            {
-                $this->signalSignature = $this->register([
-                    'config'             => $this->config,
-                    'signalCallbackHash' => ClosureHash::from($this->signalCallback)
-                ]);
-                $this->scan();
-            }
-        }
-    }
-
-    public function setupSignal(): Signal
-    {
-        $signal = new Signal();
-
-        $signal->symbol_id = $this->symbol->id;
-        $signal->indicator_id = $this->id();
-        $signal->signature_id = $this->signalSignature->id;
-
-        return $signal;
-    }
-
-    protected function checkSignal(int $timestamp, mixed $value): ?Signal
-    {
-        $callback = $this->signalCallback;
-        return $callback(signal: $this->setupSignal(),
-            indicator: $this,
-            timestamp: $timestamp,
-            value: $value);
-    }
-
     public function prev(): mixed
     {
         return $this->data[$this->prev] ?? null;
-    }
-
-    protected function scan(): void
-    {
-        foreach ($this->data as $timestamp => $value)
-        {
-            $this->current = $timestamp;
-            if ($signal = $this->checkSignal($timestamp, $value))
-            {
-                $signal->timestamp = $timestamp;
-                $signal->hash = $this->hash(json_encode($signal->attributesToArray()));
-                $existing = Signal::query()->where('hash', $signal->hash)->first();
-
-                if ($existing)
-                {
-                    $signal = $existing;
-                }
-                else
-                {
-                    $signal->save();
-                }
-
-                $this->signals[$timestamp] = $signal;
-            }
-
-            $this->prev = $timestamp;
-        }
     }
 
     public function closePrice(): float
@@ -166,28 +190,6 @@ abstract class AbstractIndicator
         return $this->candles;
     }
 
-    protected function combineTimestamps(?array $data): array
-    {
-        if (!$data)
-        {
-            return [];
-        }
-
-        $timestamps = array_slice($this->timestamps(), ($length = count($data)) * -1, $length);
-
-        return array_combine($timestamps, $data);
-    }
-
-    protected function closes(): array
-    {
-        return array_column($this->candles->all(), 'c');
-    }
-
-    protected function timestamps(): array
-    {
-        return array_column($this->candles->all(), 't');
-    }
-
     /**
      * @return Signal[]
      */
@@ -206,8 +208,8 @@ abstract class AbstractIndicator
         return $this->data;
     }
 
-    public function id(): int
+    protected function closes(): array
     {
-        return $this->signature->id;
+        return array_column($this->candles->all(), 'c');
     }
 }
