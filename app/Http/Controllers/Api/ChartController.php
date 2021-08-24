b87bf8225c0b3b3c9ffce7ba51ca936219b4aab6
milkymeda <mikailfidan38@gmail.com>
2021-08-24 16:03:09 +0300

    Handle range and strategy input

--- app/Http/Controllers/Api/ChartController.php
+++ app/Http/Controllers/Api/ChartController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Symbol;
 use App\Repositories\SymbolRepository;
+use App\Trade\StrategyTester;
 use App\Trade\Config;
 use App\Trade\Exchange\AbstractExchange;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 
 class ChartController extends Controller
 {
@@ -22,15 +26,17 @@ class ChartController extends Controller
 
         if ($exchange && $symbol && $interval)
         {
-            return $this->candles($exchange,
-                $symbol,
-                $interval,
-                $request->get('indicators', []),
-                $request->get('before'),
-                $request->get('limit'));
+            return $this->candles(exchange: $exchange,
+                symbolName: $symbol,
+                interval: $interval,
+                indicators: $request->get('indicators', []),
+                strategy: $request->get('strategy'),
+                range: json_decode($request->get('range'), true),
+                limit: $request->get('limit'));
         }
 
         return [
+            'strategies' => Config::strategies(),
             'exchanges'  => Config::exchanges(),
             'symbols'    => Config::symbols(),
             'indicators' => Config::indicators(),
@@ -42,17 +48,44 @@ class ChartController extends Controller
      * @param AbstractExchange $exchange
      */
     public function candles(string $exchange,
-                            string $symbol,
+                            string $symbolName,
                             string $interval,
                             array  $indicators,
-                            ?int   $before = null,
+                            string $strategy = null,
+                            ?array $range = null,
                             ?int   $limit = null): array
     {
-        return $this->symbolRepo->candles($exchange::instance(),
-                $symbol,
-                $interval,
-                $before,
-                $limit,
-                $indicators)?->toArray() ?? [];
+
+        $start = $range ? Carbon::parse($range['start'])->getTimestampMs() : null;
+        $end = $range ? Carbon::parse($range['end'])->getTimestampMs() : null;
+
+        if ($strategy)
+        {
+            /** @var Symbol $symbol */
+            $symbol = $this->symbolRepo->fetchSymbols(symbols: [$symbolName],
+                interval: $interval,
+                exchangeId: $exchange::instance()->id())
+                ->first();
+
+            abort_if(!$symbol, 404, "Symbol $symbolName was not found.");
+
+            $tester = new StrategyTester(App::make(SymbolRepository::class), [
+                'startDate' => $start,
+                'endDate'   => $end
+            ]);
+            $result = $tester->run($strategy, $symbol);
+
+            $symbol = $symbol->toArray();
+            $symbol['strategy'] = $result;
+            return $symbol;
+        }
+
+        return $this->symbolRepo->candles(exchange: $exchange::instance(),
+                symbol: $symbolName,
+                interval: $interval,
+                limit: $range ? null : $limit,
+                indicators: $indicators,
+                end: $end,
+                start: $start)?->toArray() ?? [];
     }
 }
