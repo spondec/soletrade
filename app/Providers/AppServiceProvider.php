c4b30e1f19cde34576c574b1fbc22d42a9bc9567
milkymeda <mikailfidan38@gmail.com>
2021-09-11 18:45:19 +0300

    Force morph map

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\Signal;
+use App\Models\TradeSetup;
 use App\Repositories\SymbolRepository;
 use App\Trade\StrategyTester;
 use App\Trade\Exchange\Spot\Binance;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
@@ -37,5 +40,10 @@ class AppServiceProvider extends ServiceProvider
         {
             \Illuminate\Support\Facades\Log::error($e->getMessage());
         }
+
+        Relation::enforceMorphMap([
+            'signal'      => Signal::class,
+            'trade_setup' => TradeSetup::class,
+        ]);
     }
 }
