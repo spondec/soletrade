f80645bf8c856d167823acf1bf47b798c1dbbc37
milkymeda <mikailfidan38@gmail.com>
2021-09-18 22:00:25 +0300

    Refactor summarizer

--- app/Trade/Evaluation/Summarizer.php
+++ app/Trade/Evaluation/Summarizer.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Trade\Evaluation;
 
 use App\Models\Evaluation;
@@ -7,6 +9,19 @@ use Illuminate\Support\Collection;
 
 class Summarizer
 {
+    protected float $balance = 100;
+
+    protected int $total = 0;
+    protected int $ambiguous = 0;
+    protected int $profit = 0;
+    protected int $loss = 0;
+    protected int $failed = 0;
+
+    protected array $lowestRois = [];
+    protected array $highestRois = [];
+
+    protected float $feeRatio = 0.001;
+
     /**
      * @param Evaluation[] $evaluations
      *
@@ -14,85 +29,35 @@ class Summarizer
      */
     public function summarize(Collection $evaluations): array
     {
-        $balance = 100;
-        $count = 0;
-
-        $ambiguous = 0;
-        $profit = 0;
-        $loss = 0;
-        $failed = 0;
-
-        $lowestRois = [];
-        $highestRois = [];
-
         foreach ($evaluations as $evaluation)
         {
-            $validEntry = $evaluation->is_entry_price_valid;
-            $isAmbiguous = $evaluation->is_ambiguous;
-            $realized = $evaluation->realized_roi;
-
-            if ($validEntry && !$isAmbiguous && $realized)
-            {
-                $realizedRois[] = $realized;
-                $balance = $this->cutCommission($balance, 0.002);
-                $pnl = $this->calculatePnl($balance, $realized);
-                $balance += $pnl;
-                $count++;
-
-                $highestRois[] = (float)$evaluation->highest_roi;
-                $lowestRois[] = (float)$evaluation->lowest_roi;
-            }
-
-            if ($isAmbiguous)
-            {
-                $ambiguous++;
-            }
-            else if (!$validEntry)
-            {
-                $failed++;
-            }
-            else if ($realized < 0)
-            {
-                $loss++;
-            }
-            else if ($realized > 0)
-            {
-                $profit++;
-            }
+            $this->processEvaluation($evaluation);
         }
 
-        $sum = [
-            'avg_roi'           => 0,
-            'avg_highest_roi'   => 0,
-            'avg_lowest_roi'    => 0,
-            'risk_reward_ratio' => 0,
-            'profit'            => $profit,
-            'loss'              => $loss,
-            'ambiguous'         => $ambiguous,
-            'failed'            => $failed
-        ];
+        $stat = $this->getStat();
+        $averages = $this->roundArray($this->calcAverages(), 2);
 
-        $sum['roi'] = $roi = round($balance - 100, 2);
+        return array_merge($stat, $averages);
+    }
 
-        if ($count > 0)//prevent division by zero
+    protected function processEvaluation(Evaluation $evaluation): void
+    {
+        $validEntry = (bool)$evaluation->is_entry_price_valid;
+        $isAmbiguous = (bool)$evaluation->is_ambiguous;
+        $realized = (float)$evaluation->realized_roi;
+
+        if ($validEntry && !$isAmbiguous && $realized)
         {
-            $sum['avg_roi'] = round($roi / $count, 2);
+            $this->balance = $this->cutCommission($this->balance, $this->feeRatio * 2);
+            $pnl = $this->calculatePnl($this->balance, $realized);
+            $this->balance += $pnl;
+            $this->total++;
 
-            if ($highestRois)
-            {
-                $sum['avg_highest_roi'] = round($avgHighestRoi = array_sum($highestRois) / $count, 2);
-            }
-            if ($lowestRois)
-            {
-                $sum['avg_lowest_roi'] = round($avgLowestRoi = array_sum($lowestRois) / $count, 2);
-            }
-            if ($highestRois && $lowestRois)
-            {
-                $sum['risk_reward_ratio'] = round(abs($avgHighestRoi / $avgLowestRoi), 2);
-            }
+            $this->highestRois[] = (float)$evaluation->highest_roi;
+            $this->lowestRois[] = (float)$evaluation->lowest_roi;
         }
 
-        return $sum;
+        $this->updateCounters($isAmbiguous, $validEntry, $realized);
     }
 
     public function cutCommission(float|int $balance, float|int $ratio): int|float
@@ -103,5 +68,81 @@ class Summarizer
     public function calculatePnl(float $balance, float $roi): float|int
     {
         return $balance * $roi / 100;
+    }
+
+    private function updateCounters(bool $isAmbiguous, bool $validEntry, float $realized): void
+    {
+        if ($isAmbiguous)
+        {
+            $this->ambiguous++;
+        }
+        else if (!$validEntry)
+        {
+            $this->failed++;
+        }
+        else if ($realized < 0)
+        {
+            $this->loss++;
+        }
+        else if ($realized > 0)
+        {
+            $this->profit++;
+        }
+    }
+
+    private function getStat(): array
+    {
+        return [
+            'fee_ratio' => $this->feeRatio,
+            'profit'    => $this->profit,
+            'loss'      => $this->loss,
+            'ambiguous' => $this->ambiguous,
+            'failed'    => $this->failed
+        ];
+    }
+
+    /**
+     * @param float[] $items
+     */
+    protected function roundArray(array $items, int $precision): array
+    {
+        foreach ($items as &$item)
+        {
+            $item = round($item, $precision);
+        }
+
+        return $items;
+    }
+
+    private function calcAverages(): array
+    {
+        $sum = [
+            'avg_roi'           => 0,
+            'avg_highest_roi'   => 0,
+            'avg_lowest_roi'    => 0,
+            'risk_reward_ratio' => 0
+        ];
+
+        $sum['roi'] = $roi = $this->balance - 100;
+
+        if ($this->total > 0)//prevent division by zero
+        {
+            $sum['avg_roi'] = $roi / $this->total;
+
+            if ($this->highestRois)
+            {
+                $sum['avg_highest_roi'] = $avgHighestRoi = array_sum($this->highestRois) / $this->total;
+            }
+            if ($this->lowestRois)
+            {
+                $sum['avg_lowest_roi'] = $avgLowestRoi = array_sum($this->lowestRois) / $this->total;
+            }
+            if ($this->highestRois && $this->lowestRois)
+            {
+                $sum['risk_reward_ratio'] = abs($avgHighestRoi / $avgLowestRoi);
+            }
+        }
+
+        return $sum;
     }
 }
