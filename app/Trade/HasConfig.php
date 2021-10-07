54132b1d9bbb05c8dea6f28ba72938da21dcd4e5
milkymeda <mikailfidan38@gmail.com>
2021-10-07 05:01:42 +0300

    Add config accessor

--- app/Trade/HasConfig.php
+++ app/Trade/HasConfig.php
@@ -11,4 +11,17 @@ trait HasConfig
             $this->config = array_merge_recursive_distinct($this->config, $config);
         }
     }
+
+    public function config(string $key): mixed
+    {
+        $keys = explode('.', $key);
+        $value = &$this->config;
+
+        foreach ($keys as $k)
+        {
+            $value = &$value[$k];
+        }
+
+        return $value;
+    }
 }
