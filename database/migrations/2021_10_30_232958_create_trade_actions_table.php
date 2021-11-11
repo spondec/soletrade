31839ceaa65bf058279ececd50f116b3394b200e
milkymeda <mikailfidan38@gmail.com>
2021-11-11 21:17:21 +0300

    Create 2021_10_30_232958_create_trade_actions_table.php

---
+++ database/migrations/2021_10_30_232958_create_trade_actions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use App\Illuminate\Database\Schema\Blueprint;
+use App\Illuminate\Support\Facades\Schema;
+
+class CreateTradeActionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trade_actions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('trade_setup_id');
+            $table->string('class');
+            $table->json('config');
+            $table->boolean('is_taken')->default(false);
+            $table->bigInteger('timestamp')->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trade_actions');
+    }
+}
