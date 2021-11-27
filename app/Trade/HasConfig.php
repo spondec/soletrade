f3f1c4ee8871c7c2dc60714c01f0d05838a59a26
milkymeda <mikailfidan38@gmail.com>
2021-11-28 02:26:41 +0300

    Assert key match

--- app/Trade/HasConfig.php
+++ app/Trade/HasConfig.php
@@ -8,15 +8,37 @@ trait HasConfig
     {
         if (method_exists($this, 'getDefaultConfig'))
         {
-            $defaultConfig = $this->getDefaultConfig();
-            $childConfig = $this->config;
+            $defaultConfig = $this->getDefaultConfig(); //defined by an abstract class, preferably
+            $childConfig = $this->config;               //defined by the instance class or inherited
 
+            //changes to config keys from the child class via $config property are expected
+            //so do not try to match keys here
             $this->config = array_merge_recursive_distinct($defaultConfig, $childConfig);
+
+            //additions to config keys are not allowed, will match keys now
+            $this->assertKeyMatch($this->config, $config);
             $this->config = array_merge_recursive_distinct($this->config, $config);
         }
         else
         {
+            $this->assertKeyMatch($this->config, $config);
             $this->config = array_merge_recursive_distinct($this->config, $config);
+        }
+    }
+
+    protected function assertKeyMatch(array &$original, array &$replacement)
+    {
+        foreach ($replacement as $key => &$value)
+        {
+            if (!array_key_exists($key, $original))
+            {
+                throw new \UnexpectedValueException("Config key does match: $key");
+            }
+
+            if (is_array($value))
+            {
+                $this->assertKeyMatch($original[$key], $value);
+            }
         }
     }
 
