8057c232553767cfa638d1640217bf231cbd48bc
milkymeda <mikailfidan38@gmail.com>
2021-12-02 14:38:49 +0300

    update getter visibility

--- app/Trade/CandleCollection.php
+++ app/Trade/CandleCollection.php
@@ -7,23 +7,23 @@ use JetBrains\PhpStorm\Pure;
 
 class CandleCollection extends Collection
 {
-    #[Pure] protected function closes(): array
+    #[Pure] public function closes(): array
     {
         return array_column($this->all(), 'c');
     }
 
-    #[Pure] protected function highs(): array
+    #[Pure] public function highs(): array
 
     {
         return array_column($this->all(), 'h');
     }
 
-    #[Pure] protected function lows(): array
+    #[Pure] public function lows(): array
     {
         return array_column($this->all(), 'l');
     }
 
-    #[Pure] protected function timestamps(): array
+    #[Pure] public function timestamps(): array
     {
         return array_column($this->all(), 't');
     }
