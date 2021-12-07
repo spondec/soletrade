c3adf190f99446a7a9fdf543585f8723353795fc
mf <mikailfidan38@gmail.com>
2021-12-08 01:52:02 +0300

    Wrapper methods for Binder

--- app/Models/Binding.php
+++ app/Models/Binding.php
@@ -3,30 +3,32 @@
 namespace App\Models;
 
 use App\Trade\Binding\Bindable;
+use App\Trade\Binding\Binder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 /**
- * @property Bindable                                   bindable
- * @property SavePoint[]|\Illuminate\Support\Collection savePoints
- * @property Signature                                  signature
+ * @property Bindable                   bindable
+ * @property Signature                  signature
  *
- * @property int                                        id
- * @property string                                     bindable_type
- * @property int                                        bindable_id
- * @property int                                        signature_id
- * @property string                                     class
- * @property string                                     column
- * @property string                                     name
+ * @property int                        id
+ * @property string                     bindable_type
+ * @property int                        bindable_id
+ * @property int                        signature_id
+ * @property string                     class
+ * @property string                     column
+ * @property string                     name
  *
- * @property \Illuminate\Support\Carbon                 created_at
- * @property \Illuminate\Support\Carbon                 updated_at
+ * @property \Illuminate\Support\Carbon created_at
+ * @property \Illuminate\Support\Carbon updated_at
  */
 class Binding extends Model
 {
     protected $guarded = ['id'];
     protected array $unique = ['bindable_type', 'bindable_id', 'column'];
+
+    public readonly Binder $binder;
+    public readonly ?\Closure $callback;
 
     public function bindable(): MorphTo
     {
@@ -38,8 +40,15 @@ class Binding extends Model
         return $this->belongsTo(Signature::class);
     }
 
-    public function savePoints(): HasMany
+    public function setBinder(Binder $binder, ?\Closure $callback): void
     {
-        return $this->hasMany(SavePoint::class, 'binding_signature_id', 'signature_id');
+        $this->binder = $binder;
+        $this->callback = $callback;
+    }
+
+    public function getBindValue(int $timestamp, ...$params)
+    {
+        $value = $this->binder->getBindValue($this->name, $timestamp, ...$params);
+        return $this->callback ? ($this->callback)($value) : $value;
     }
 }
