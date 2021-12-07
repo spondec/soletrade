726272ab86c9fdc7c63e2f5f71a52856d5ed4493
mf <mikailfidan38@gmail.com>
2021-12-08 01:45:21 +0300

    Implement prevCandles()

--- app/Trade/CandleCollection.php
+++ app/Trade/CandleCollection.php
@@ -27,4 +27,51 @@ class CandleCollection extends Collection
     {
         return array_column($this->all(), 't');
     }
+
+    public function previousCandles(int $amount, int $startIndex): static
+    {
+        if ($amount > $startIndex)
+        {
+            throw new \InvalidArgumentException('Not enough candles exist.');
+        }
+
+        $candles = [];
+        for ($i = 1; $i <= $amount; $i++)
+        {
+            $candles[] = $this->items[$startIndex - $i];
+        }
+        return new static(array_reverse($candles, false));
+    }
+
+    public function findPrevNext(int        $timestamp,
+                                 ?\stdClass &$prev = null,
+                                 ?\stdClass &$next = null,
+                                 ?int       &$prevKey = null,
+                                 ?int       &$nextKey = null): void
+    {
+        /**
+         * @var int       $key
+         * @var \stdClass $candle
+         */
+        foreach ($this->items as $key => $candle)
+        {
+            if (!$prev)
+            {
+                if ($candle->t > $timestamp)
+                {
+                    $prev = $_prev ?? null;
+                    $prevKey = $_prevKey ?? null;
+                }
+            }
+            else if ($candle->t > $prev->t)
+            {
+                $next = $candle;
+                $nextKey = $key;
+                break;
+            }
+
+            $_prev = $candle;
+            $_prevKey = $key;
+        }
+    }
 }
---
+++ tests/Unit/Trade/CandleCollectionTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit\Trade;
+
+use App\Trade\CandleCollection;
+use PHPUnit\Framework\TestCase;
+
+class CandleCollectionTest extends TestCase
+{
+    public function test_previous_candles()
+    {
+        $collection = new CandleCollection(range(1, 10));
+
+        $res = $collection->previousCandles($count = 5, 5);
+
+        $this->assertEquals(4, $res[0]);
+        $this->assertEquals(5, $res[1]);
+        $this->assertCount($count, $res);
+    }
+
+    public function test_previous_candles_throws_exception()
+    {
+        $collection = new CandleCollection(range(1, 10));
+
+        $this->expectExceptionMessage('Not enough candles exist');
+        $collection->previousCandles(3, 2);
+    }
+}
