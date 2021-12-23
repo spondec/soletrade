d0f25e8dcc3211914720a8a9cc0dc954ec09ff9c
mf <mikailfidan38@gmail.com>
2021-12-23 03:44:34 +0300

    remove test code

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,21 +2,10 @@
 
 namespace Tests\Feature;
 
-use App\Models\Symbol;
-use App\Repositories\SymbolRepository;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    public function inttest(int $test)
-    {
-
-    }
-
-    public function test_bug_case()
-    {
-    }
-
     /**
      * A basic test example.
      *
@@ -24,19 +13,6 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-
-        $repo = new SymbolRepository();
-
-//        Binance::instance()->updater()->updateByInterval('1m', 1);
-
-        /** @var Symbol $symbol */
-        $symbol = Symbol::query()
-            ->where('symbol', 'BTC/USDT')
-            ->where('interval', '1m')
-            ->first();
-
-        $repo->fetchSymbolFromExchange($symbol->exchange(), $symbol->symbol, $symbol->interval);
-        $symbol->exchange()->updater()->update($symbol);
     }
 }
 
