bf05de4c6308c0eaf14012666cbbe2b9c4a369f7
mf <mikailfidan38@gmail.com>
2022-02-07 18:43:20 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,6 @@ use App\Models\Signal;
 use App\Models\TradeSetup;
 use App\Repositories\SymbolRepository;
 use App\Trade\StrategyTester;
-use App\Trade\Exchange\Spot\Binance;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
@@ -33,14 +32,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        try
-        {
-            Binance::instance();
-        } catch (\Exception $e)
-        {
-            \Illuminate\Support\Facades\Log::error($e->getMessage());
-        }
-
         Relation::enforceMorphMap([
             'signal'      => Signal::class,
             'trade_setup' => TradeSetup::class,
