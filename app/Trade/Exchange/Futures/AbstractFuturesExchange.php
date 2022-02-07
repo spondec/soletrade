e78c5e9a01f669b58f7fcd1ed0535b82129c6479
mf <mikailfidan38@gmail.com>
2022-02-07 23:48:26 +0300

    Delete stubs

--- app/Trade/Exchange/Futures/AbstractFuturesExchange.php
+++ app/Trade/Exchange/Futures/AbstractFuturesExchange.php
@@ -2,26 +2,9 @@
 
 namespace App\Trade\Exchange\Futures;
 
-use App\Models\Position;
 use App\Trade\Exchange\AbstractExchange;
 
 abstract class AbstractFuturesExchange extends AbstractExchange
 {
-    /**
-     * @return Position[]
-     *
-     */
-    abstract public function openPositions(): array;
-
-    abstract public function markPrice(string $symbol): float;
-
-    public function short()
-    {
-
-    }
-
-    public function long()
-    {
-
-    }
+    
 }
