2a357c9a672b668d9b7c3ddb4799988460601a65
mf <mikailfidan38@gmail.com>
2022-02-07 19:00:32 +0300

    Refactor

--- app/Trade/Evaluation/Summarizer.php
+++ app/Trade/Evaluation/Summarizer.php
@@ -7,60 +7,55 @@ namespace App\Trade\Evaluation;
 use App\Models\Evaluation;
 use App\Models\Summary;
 use App\Trade\Calc;
-use App\Trade\Strategy\AbstractStrategy;
+use App\Trade\Strategy\Strategy;
 use Illuminate\Support\Collection;
 
 class Summarizer
 {
+    protected readonly Summary $summary;
+
+    protected float $balance = 100;
+    protected array $balanceHistory = [];
+
+    protected float $feeRatio;
+    protected float $totalFee = 0;
+
     protected array $highestRoi = [];
     protected array $lowestRoi = [];
-
     protected array $profitRoi = [];
     protected array $lossRoi = [];
 
-    protected float $balance = 100;
-
-    protected float $feeRatio;
-
-    protected array $balanceHistory = [];
-
-    protected float $totalFee = 0;
-
-    public function __construct(protected AbstractStrategy $strategy)
+    public function __construct(protected Strategy $strategy)
     {
         $this->feeRatio = $this->strategy->config('feeRatio');
+        $this->summary = $this->setupSummary();
     }
 
-    public function strategy(): AbstractStrategy
+    protected function setupSummary(): Summary
     {
-        return $this->strategy;
-    }
-
-    /**
-     * @param Evaluation[] $evaluations
-     *
-     * @return array
-     */
-    public function summarize(Collection $evaluations): Summary
-    {
-        $summary = $this->setupSummary();
-        $evaluations->map(fn(Evaluation $evaluation): Evaluation => $this->processEvaluation($evaluation, $summary));
-
-        $this->calcAverages($summary);
+        $summary = new Summary();
+        $summary->fee_ratio = $this->feeRatio;
 
         return $summary;
     }
 
-    protected function recordBalance(int $timestamp)
+    public function strategy(): Strategy
     {
-        if (array_key_exists($timestamp, $this->balanceHistory))
-        {
-            throw new \LogicException("Balance record for timestamp {$timestamp} already exists.");
-        }
-        $this->balanceHistory[$timestamp] = $this->balance;
+        return $this->strategy;
     }
 
-    protected function processEvaluation(Evaluation $evaluation, Summary $summary): Evaluation
+    /** @param Evaluation[] $evaluations */
+    public function summarize(Collection $evaluations): Summary
+    {
+        foreach ($evaluations as $evaluation)
+        {
+            $this->addEvaluation($evaluation);
+        }
+
+        return $this->getSummary();
+    }
+
+    public function addEvaluation(Evaluation $evaluation): void
     {
         $isEntryValid = (bool)$evaluation->is_entry_price_valid;
         $isAmbiguous = (bool)$evaluation->is_ambiguous;
@@ -70,14 +65,14 @@ class Summarizer
         {
             if (!$this->balanceHistory)
             {
-                $this->recordBalance($evaluation->entry_timestamp);
+                $this->recordBalance($evaluation->entry_timestamp); //registers initial balance
             }
             $this->cutCommission($evaluation->used_size, $this->feeRatio * 2);
             $pnl = Calc::pnl($this->balance, $relativeRoi);
 
             $this->balance += $pnl;
             $this->recordBalance($evaluation->exit_timestamp);
-            $summary->total++;
+            $this->summary->total++;
 
             if ($relativeRoi > 0)
             {
@@ -91,9 +86,22 @@ class Summarizer
             $this->lowestRoi[] = (float)$evaluation->lowest_roi;
         }
 
-        $this->updateCounters($summary, $isAmbiguous, $isEntryValid, $relativeRoi);
+        $this->updateCounters($isAmbiguous, $isEntryValid, $relativeRoi);
+    }
 
-        return $evaluation;
+    protected function recordBalance(int $timestamp): void
+    {
+        if (array_key_exists($timestamp, $this->balanceHistory))
+        {
+            throw new \LogicException("Balance record for timestamp $timestamp already exists.");
+        }
+
+        if (array_key_last($this->balanceHistory) > $timestamp)
+        {
+            throw new \LogicException("New balance record can't be older than previous record.");
+        }
+
+        $this->balanceHistory[$timestamp] = $this->balance;
     }
 
     protected function cutCommission(float $usedSize, float|int $ratio): void
@@ -105,66 +113,68 @@ class Summarizer
         $this->balance -= $fee;
     }
 
-    private function updateCounters(Summary $summary, bool $isAmbiguous, bool $isEntryValid, float $roi): void
+    protected function updateCounters(bool $isAmbiguous, bool $isEntryValid, float $roi): void
     {
         if ($isAmbiguous)
         {
-            $summary->ambiguous++;
+            $this->summary->ambiguous++;
         }
         else if (!$isEntryValid)
         {
-            $summary->failed++;
+            $this->summary->failed++;
         }
         else if ($roi < 0)
         {
-            $summary->loss++;
+            $this->summary->loss++;
         }
         else if ($roi > 0)
         {
-            $summary->profit++;
+            $this->summary->profit++;
         }
     }
 
-    protected function setupSummary(): Summary
+    public function getSummary(): Summary
     {
-        $summary = new Summary();
-        $summary->fee_ratio = $this->feeRatio;
+        $this->calcAverages();
 
-        return $summary;
+        return $this->summary;
     }
 
-    private function calcAverages(Summary $summary): void
+    protected function calcAverages(): void
     {
+        $summary = $this->summary;
+
+        if ($summary->total == 0)
+        {
+            return;
+        }
+
         $summary->roi = round($roi = $this->balance - 100, 2);
         $summary->total_fee = $this->totalFee;
         $summary->balance_history = $this->balanceHistory;
+        $summary->avg_roi = round($roi / $summary->total, 2);
+        $summary->success_ratio = round($summary->profit / $summary->total * 100, 2);
 
-        if ($summary->total > 0)//prevent division by zero
+        if ($this->profitRoi)
         {
-            $summary->avg_roi = round($roi / $summary->total, 2);
-            $summary->success_ratio = round($summary->profit / $summary->total * 100, 2);
+            $summary->avg_profit_roi = round(Calc::avg($this->profitRoi), 2);
+        }
+        if ($this->lossRoi)
+        {
+            $summary->avg_loss_roi = round(Calc::avg($this->lossRoi), 2);
+        }
 
-            if ($this->profitRoi)
-            {
-                $summary->avg_profit_roi = round(Calc::avg($this->profitRoi), 2);
-            }
-            if ($this->lossRoi)
-            {
-                $summary->avg_loss_roi = round(Calc::avg($this->lossRoi), 2);
-            }
-
-            if ($this->highestRoi)
-            {
-                $summary->avg_highest_roi = round(Calc::avg($this->highestRoi), 2);
-            }
-            if ($this->lowestRoi)
-            {
-                $summary->avg_lowest_roi = round(Calc::avg($this->lowestRoi), 2);
-            }
-            if ($this->profitRoi && $this->lossRoi)
-            {
-                $summary->risk_reward_ratio = round(abs($summary->avg_profit_roi / $summary->avg_loss_roi), 2);
-            }
+        if ($this->highestRoi)
+        {
+            $summary->avg_highest_roi = round(Calc::avg($this->highestRoi), 2);
+        }
+        if ($this->lowestRoi)
+        {
+            $summary->avg_lowest_roi = round(Calc::avg($this->lowestRoi), 2);
+        }
+        if ($this->profitRoi && $this->lossRoi)
+        {
+            $summary->risk_reward_ratio = round(abs($summary->avg_profit_roi / $summary->avg_loss_roi), 2);
         }
     }
 }
