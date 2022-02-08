606ee374264fd3893df291d77840c51541223f55
mf <mikailfidan38@gmail.com>
2022-02-08 16:04:55 +0300

    Define property types and make it readonly

--- app/Trade/CandleMap.php
+++ app/Trade/CandleMap.php
@@ -4,7 +4,12 @@ namespace App\Trade;
 
 class CandleMap
 {
-    public function __construct(public $t, public $o, public $c, public $h, public $l, public $v)
+    public function __construct(public readonly int|string $t,
+                                public readonly int|string $o,
+                                public readonly int|string $c,
+                                public readonly int|string $h,
+                                public readonly int|string $l,
+                                public readonly int|string $v)
     {
     }
 }
