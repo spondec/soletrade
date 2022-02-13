fe195526cc4594e2f539148c74a2bee214b0b0d2
mf <mikailfidan38@gmail.com>
2022-02-14 00:39:28 +0300

    Add withSignals field, remove symbol

--- app/Trade/Config/TradeConfig.php
+++ app/Trade/Config/TradeConfig.php
@@ -3,14 +3,15 @@
 namespace App\Trade\Config;
 
 use App\Models\Signature;
-use App\Models\Symbol;
 
 class TradeConfig extends Config
 {
+    public readonly bool $withSignals;
+
     public function __construct(public readonly array $signals,
                                 public readonly \Closure $callback,
-                                public readonly Signature $signature,
-                                public readonly Symbol $symbol)
+                                public readonly Signature $signature)
     {
+        $this->withSignals = !empty($signals);
     }
 }
