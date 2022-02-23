4d4668deed195f5cbd12cde9f0ff0fa0023732c9
mf <mikailfidan38@gmail.com>
2022-02-24 00:13:16 +0300

    Add CandleFactory

--- database/factories/CandleFactory.php
+++ database/factories/CandleFactory.php
@@ -2,11 +2,31 @@
 
 namespace Database\Factories;
 
+use App\Trade\Calc;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Collection;
 
 class CandleFactory extends Factory
 {
     protected $model = \App\Models\Candle::class;
+
+    public function __construct($count = null,
+                                ?Collection $states = null,
+                                ?Collection $has = null,
+                                ?Collection $for = null,
+                                ?Collection $afterMaking = null,
+                                ?Collection $afterCreating = null,
+        $connection = null,
+
+                                protected ?int $interval = null,
+                                protected ?Carbon $startDate = null,
+                                protected ?float $priceLowerThan = null,
+                                protected ?float $priceHigherThan = null,
+                                protected ?int $lastTimestamp = null)
+    {
+        parent::__construct($count, $states, $has, $for, $afterMaking, $afterCreating, $connection);
+    }
 
     /**
      * Define the model's default state.
@@ -15,13 +35,91 @@ class CandleFactory extends Factory
      */
     public function definition(): array
     {
+        $prices = [];
+        for ($i = 0; $i < 4; $i++)
+        {
+            $prices[] = $this->randomPrice(2, 1, 100);
+        }
+
         return [
-            't' => $this->faker->dateTimeBetween('-1 year', 'now')->getTimestamp() * 1000,
-            'o' => $this->faker->randomFloat(2, 0, 100),
-            'c' => $this->faker->randomFloat(2, 0, 100),
-            'h' => $this->faker->randomFloat(2, 0, 100),
-            'l' => $this->faker->randomFloat(2, 0, 100),
-            'v' => $this->faker->randomFloat(2, 0, 100),
+            't' => $this->startDate && $this->interval
+                ? $this->getNextTimestamp()
+                : $this->faker
+                    ->dateTimeBetween($this->startDate ?: '-1 year', 'now')
+                    ->getTimestamp() * 1000,
+            'o' => $prices[\array_rand($prices)],
+            'c' => $prices[\array_rand($prices)],
+            'h' => \max($prices),
+            'l' => \min($prices),
+            'v' => $this->faker->randomFloat(2, 100, 100 * 10),
         ];
+    }
+
+    /**
+     * @param int $startDate
+     * @param int $endDate
+     * @param int $interval in seconds
+     *
+     * @return $this
+     */
+    public function fillBetween(int $startDate, int $endDate, int $interval): static
+    {
+        $this->interval = $interval * 1000;
+        $this->startDate = Carbon::createFromTimestampMs($startDate = Calc::asMs($startDate) - $this->interval);
+
+        $endDate = Calc::asMs($endDate);
+
+        $this->count = 0;
+
+        while ($startDate < $endDate)
+        {
+            $startDate += $this->interval;
+            $this->count++;
+        }
+        return $this;
+    }
+
+    protected function newInstance(array $arguments = [])
+    {
+        //TODO:: dirty
+        //When laravel min php requirement bumps to 8.1,
+        //PR to remove the need of this via using
+        //the array unpacking with string keys feature
+        //at parent::newInstance()
+        $arguments['interval'] = $this->interval;
+        $arguments['startDate'] = $this->startDate;
+        $arguments['priceLowerThan'] = $this->priceLowerThan;
+        $arguments['priceHigherThan'] = $this->priceHigherThan;
+        $arguments['lastTimestamp'] = $this->lastTimestamp;
+
+        return parent::newInstance($arguments);
+    }
+
+    public function priceLowerThan(float $price): static
+    {
+        $this->priceLowerThan = $price - $price * 0.001;
+        return $this;
+    }
+
+    public function priceHigherThan(float $price): static
+    {
+        $this->priceHigherThan = $price + $price * 0.001;
+        return $this;
+    }
+
+    protected function randomPrice(int $maxDecimals, float $min, float $max): float|int
+    {
+        return $this->faker->randomFloat($maxDecimals,
+            $this->priceHigherThan ?? $min,
+            $this->priceLowerThan ?? $max);
+    }
+
+    protected function getNextTimestamp(): int
+    {
+        if (!$this->lastTimestamp)
+        {
+            $this->lastTimestamp = $this->startDate->getTimestampMs();
+        }
+        return $this->lastTimestamp += $this->interval;
     }
 }
---
+++ tests/Feature/Factories/CandleFactoryTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Factories;
+
+use App\Models\Symbol;
+use Database\Factories\CandleFactory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CandleFactoryTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_fill_between()
+    {
+        $time = time();
+        $interval = 60;
+        $symbol = Symbol::factory()->create();
+
+        $factory = $this->candleFactory();
+        $candles = $factory
+            ->for($symbol)
+            ->fillBetween($time - 600, $time, $interval)
+            ->create();
+
+        $this->assertEquals($time - 600, $candles->first()->t / 1000);
+        $this->assertEquals($time, $candles->last()->t / 1000);
+
+        $prev = $candles->shift();
+        $iterator = $candles->getIterator();
+
+        while ($iterator->valid())
+        {
+            $this->assertEquals($interval * 1000, $iterator->current()->t - $prev->t);
+
+            $prev = $iterator->current();
+            $iterator->next();
+        }
+    }
+
+    protected function candleFactory(): CandleFactory
+    {
+        return new CandleFactory();
+    }
+
+    public function test_price_higher_lower_than()
+    {
+        $candles = $this->candleFactory()
+            ->for(Symbol::factory()->create())
+            ->priceHigherThan(8)
+            ->priceLowerThan(10)
+            ->count(4)
+            ->create();
+
+        foreach ($candles as $candle)
+        {
+            $this->assertTrue(in_array((int)((string)$candle->c)[0], [8, 9]));
+            $this->assertTrue(in_array((int)((string)$candle->o)[0], [8, 9]));
+            $this->assertTrue(in_array((int)((string)$candle->h)[0], [8, 9]));
+            $this->assertTrue(in_array((int)((string)$candle->l)[0], [8, 9]));
+        }
+    }
+}
