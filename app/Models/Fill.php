9ba1368931e99ae11c1b8e86479a92fe3c90dd7e
mf <mikailfidan38@gmail.com>
2022-03-20 21:12:00 +0300

    Define unique fields

--- app/Models/Fill.php
+++ app/Models/Fill.php
@@ -6,18 +6,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 /**
  * @property int    order_id
+ * @property int    trade_id
  * @property int    timestamp
  * @property float  size
  * @property float  price
  * @property string commission_asset
  * @property float  commission
- * @property int    trade_id
  */
 class Fill extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
+    protected array $unique = ['order_id', 'trade_id'];
 
     protected static function booted()
     {
