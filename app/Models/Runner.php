4ce17e17e0008f947c989a729bd152fbf477a85f
mf <mikailfidan38@gmail.com>
2022-04-06 10:57:43 +0300

    Add Runner

---
+++ app/Models/Runner.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int    id
+ * @property string name
+ * @property int    start_date
+ * @property int    expire_date
+ */
+class Runner extends Model
+{
+    use HasFactory;
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->start_date = time();
+    }
+
+    public function setExpiry(int $seconds): static
+    {
+        $this->expire_date = time() + $seconds;
+        return $this;
+    }
+
+    public function lengthenExpiry(int $seconds): static
+    {
+        $this->expire_date += $seconds;
+        return $this;
+    }
+}
---
+++ database/migrations/2022_04_05_093609_create_runners_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use App\Illuminate\Database\Schema\Blueprint;
+use App\Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('runners', function (Blueprint $table) {
+            $table->id();
+            $table->integer('start_date');
+            $table->integer('expire_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('runners');
+    }
+};
