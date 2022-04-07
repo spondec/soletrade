1d1b464c028c549634976bf02a362c042cb26344
mf <mikailfidan38@gmail.com>
2022-04-07 15:08:42 +0300

    Introduce recoverable processes

---
+++ app/Trade/Process/Recoverable.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Trade\Process;
+
+class Recoverable
+{
+    public function __construct(protected readonly \Closure $process,
+                                public    readonly int $retryInSeconds,
+                                public    readonly int $retryLimit,
+                                public    readonly array $handle = [])
+    {
+        if (!$this->handle)
+        {
+            throw new \LogicException('No Throwable to handle.');
+        }
+    }
+
+    public function run(): mixed
+    {
+        return $this->try($this->retryInSeconds, $this->retryLimit);
+    }
+
+    protected function try(int $retryInSeconds, int $retryLimit): mixed
+    {
+        try
+        {
+            return ($this->process)();
+        } catch (\Throwable $e)
+        {
+            if ($this->isHandled($e) && $this->retryLimit > 0)
+            {
+                sleep($this->retryInSeconds);
+                $this->handle($e);
+                return $this->try($retryInSeconds, $retryLimit - 1);
+            }
+
+            throw $e;
+        }
+    }
+
+    private function isHandled(\Throwable $e): bool
+    {
+        foreach ($this->handle as $throwable)
+        {
+            if ($e::class == $throwable || is_subclass_of($e, $throwable))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    protected function handle(\Throwable $e): void
+    {
+
+    }
+}
---
+++ tests/Unit/Trade/Process/RecoverableTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Trade\Process;
+
+use App\Trade\Process\Recoverable;
+use PHPUnit\Framework\TestCase;
+
+class RecoverableTest extends TestCase
+{
+    public function test_empty_handle_throws_exception()
+    {
+        $this->expectException(\LogicException::class);
+        $this->expectExceptionMessage('No Throwable');
+        new Recoverable(fn() => $this->assertTrue(true), 1, 1);
+    }
+
+    public function test_retry_in_seconds()
+    {
+        $recoverable = new Recoverable(function () {
+            static $time;
+
+            if (!$time)
+            {
+                $time = time();
+                throw new \Exception('Failed');
+            }
+
+            $this->assertEquals(time(), $time + 1);
+        }, 1, 1, [\Exception::class]);
+
+        $recoverable->run();
+    }
+
+    public function test_retry_limit()
+    {
+        $recoverable = new Recoverable(function () {
+            static $count;
+
+            $count++;
+
+            if ($count < 2)
+            {
+                throw new \Exception('Failed');
+            }
+
+            $this->assertEquals(2, $count);
+        }, 1, 2, [\Exception::class]);
+
+        $recoverable->run();
+    }
+
+    public function test_unhandled_exception_gets_thrown()
+    {
+        $this->expectError();
+        $recoverable = new Recoverable(function () {
+            throw new \Error('Failed');
+        }, 1, 1, [\Exception::class]);
+
+        $recoverable->run();
+    }
+
+    public function test_subclass_throwable()
+    {
+        $recoverable = new Recoverable(function () {
+            static $count;
+            if (!$count)
+            {
+                $count = 1;
+                throw new \LogicException('Failed');
+            }
+            $this->assertEquals(1, $count);
+        }, 1, 1, [\Exception::class]);
+
+        $recoverable->run();
+    }
+}
