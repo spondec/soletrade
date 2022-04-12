b044288f4cacca706320ba42762cfc8e75760776
mf <mikailfidan38@gmail.com>
2022-04-12 16:12:53 +0300

    Fix asset reallocation

--- app/Trade/TradeAsset.php
+++ app/Trade/TradeAsset.php
@@ -13,8 +13,8 @@ class TradeAsset
 
     public function registerRoi(float $roi): void
     {
-        $amount = $this->allocation->amount();
-        $this->allocation->allocate($amount - Calc::pnl($amount, $roi));
+        $amount = $this->allocation->amount() / $this->allocation->leverage;
+        $this->allocation->allocate($amount + Calc::pnl($amount, $roi));
     }
 
     public function proportional(float $realSize): float
---
+++ tests/Unit/Trade/TradeAssetTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Trade;
+
+use App\Trade\AllocatedAsset;
+use App\Trade\TradeAsset;
+use Mockery as m;
+
+/**
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
+class TradeAssetTest extends m\Adapter\Phpunit\MockeryTestCase
+{
+    public function test_register_roi()
+    {
+        $allocation = m::mock('alias:' . AllocatedAsset::class);
+
+        $allocation
+            ->shouldReceive('amount')
+            ->andReturns(100);
+
+        $allocation->leverage = 5;
+
+        $allocation->shouldReceive('allocate')
+            ->once()
+            ->with(30);
+
+        $tradeAsset = new TradeAsset($allocation);
+
+        $tradeAsset->registerRoi(50);
+
+        $allocation->shouldReceive('allocate')
+            ->once()
+            ->with(10);
+
+        $tradeAsset->registerRoi(-50);
+    }
+}
