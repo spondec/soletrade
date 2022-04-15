276e22919329db0dfbb794beebb1418084504838
mf <mikailfidan38@gmail.com>
2022-04-16 00:06:28 +0300

    Update Blueprint namespace

--- database/migrations/2021_08_15_172957_create_signal_trade_setup_table.php
+++ database/migrations/2021_08_15_172957_create_signal_trade_setup_table.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Illuminate\Database\Schema\Blueprint;
 use App\Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
