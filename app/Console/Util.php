b1985edc96c20a3c89148b35fa3e6b237353f6b0
mf <mikailfidan38@gmail.com>
2022-04-19 16:10:33 +0300

    Add Util

---
+++ app/Console/Util.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Console;
+
+class Util
+{
+    public static function formatRoi(float $roi): string
+    {
+        $rounded = round($roi, 2);
+
+        if ($roi > 0)
+        {
+            return "<fg=red>$rounded%</>";
+        }
+
+        if ($roi < 0)
+        {
+            return "<fg=red>$rounded%</>";
+        }
+
+        return "$rounded%";
+    }
+
+    public static function memoryUsage(): string
+    {
+        return (int)(memory_get_usage(true) / 1024 / 1024) . 'MB';
+    }
+}
