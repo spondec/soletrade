74c79aabe8fd7f1a0d6d7ed961f79b5b31c50065
spondec <mikailfidan38@gmail.com>
2022-05-21 20:06:23 +0300

    Remove moved methods

--- app/Console/Commands/StrategyTester.php
+++ app/Console/Commands/StrategyTester.php
@@ -110,76 +110,6 @@ class StrategyTester extends TradeCommand
         return 0;
     }
 
-    protected function assertStrategyExists(string $strategyName): void
-    {
-        if (!strategy_exists($name = $strategyName))
-        {
-            $this->error("Strategy $name not found.");
-            $this->info('Available strategies:');
-            foreach (get_strategies() as $name => $class)
-            {
-                $this->info($name);
-            }
-            exit(1);
-        }
-    }
-
-    protected function assertSymbol(SymbolRepository $repo, array $args): Symbol
-    {
-        $symbol = $repo->fetchSymbol(Exchange::from($args['exchange']),
-            $args['symbol'],
-            $args['interval']);
-
-        if (!$symbol)
-        {
-            $this->error("Symbol {$args['symbol']} {$args['interval']} not found on {$args['exchange']}.");
-            exit(1);
-        }
-
-        return $symbol;
-    }
-
-    /**
-     * @param array $options
-     *
-     * @return int[]
-     */
-    protected function getDateRange(array $options): array
-    {
-        try
-        {
-            $startDate = $options['start']
-                ? $this->newDate($options['start'])
-                    ->setTime(0, 0)
-                    ->getTimestamp() * 1000
-                : null;
-
-            $endDate = $options['end']
-                ? $this->newDate($options['end'])
-                    ->setTime(23, 59, 59)
-                    ->getTimestamp() * 1000
-                : null;
-        } catch (InvalidFormatException $e)
-        {
-            $this->error($e->getMessage());
-            $this->error("Invalid date format.");
-            $this->error("Use format d-m-Y as in 19-11-2021.");
-            exit(1);
-        }
-
-        if ($startDate && !$endDate)
-        {
-            $endDate = Carbon::now()->setTime(0, 0)->getTimestamp() * 1000;
-        }
-
-        return [$startDate, $endDate];
-    }
-
-    protected function newDate(string $date): Carbon
-    {
-        return Carbon::createFromFormat('d-m-Y', $date);
-    }
-
     protected function initSections(): void
     {
         $this->sections['state'] = $this->newOutputSection();
@@ -197,13 +127,6 @@ class StrategyTester extends TradeCommand
         $this->helpers['table']['eval'] = new Table($this->sections['evalTable']);
         $this->helpers['table']['eval']->setHorizontal();
         $this->helpers['table']['optSummary'] = new Table($this->sections['optSummaryTable']);
-    }
-
-    protected function newOutputSection(): ConsoleSectionOutput
-    {
-        /** @var ConsoleOutput $output */
-        $output = $this->output->getOutput();
-        return $output->section();
     }
 
     protected function updateSymbols(Strategy $strategy): void
